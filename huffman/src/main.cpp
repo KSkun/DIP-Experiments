fb93fa341922eb54e1ba89efff6d909d1fa7c6d6
KSkun <ks@ksmeow.moe>
2022-04-21 22:49:38 +0800

    feat: app icon

--- huffman/src/main.cpp
+++ huffman/src/main.cpp
@@ -4,6 +4,8 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    QIcon icon(":/icon.png");
+    app.setWindowIcon(icon);
     MainWindow mainWindow;
     mainWindow.show();
     return app.exec();
