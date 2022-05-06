f6e45f218e80d5e57fc277cec11fcf5c44aabc34
KSkun <ks@ksmeow.moe>
2022-05-06 17:30:58 +0800

    chore: init electron project

---
+++ canny/canny-ui/main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadURL("https://baidu.com")
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
